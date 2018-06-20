efa2304c8d8b65631da87f82c429ca3298eb86bd
Andrew Verenich <kakxosh@yandex.ru>
2018-06-20 09:28:45 +0300

    init mvc

--- src/main/java/by/andver/controllers/HomeController.java
+++ src/main/java/by/andver/controllers/HomeController.java
@@ -1,10 +1,7 @@
 package by.andver.controllers;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,11 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    private static final Logger logger= Logger.getLogger(HomeController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model){
-        logger.info("aaaaaaaaaa");
         return "index";
     }
 }
